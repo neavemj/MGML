7d1f5e1557231eb136dba390f4c5562ff4808b66
neavemj <matthewjneave1@gmail.com>
2016-07-28 11:22:00 +1000

    initial commit using random forest classifier

---
+++ MGML.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 10:48:46 2016
+
+@author: nea040
+"""
+
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+# import data as numpy arrays
+
+train_file = open("./data/MGML_train.txt", "rb")
+test_file = open("./data/MGML_test.txt", "rb")
+
+def create_numpy_array(data_file):
+    test_answers = []
+    data = []
+    for line in data_file:
+        line = line.strip()
+        cols = line.split("\t")
+        test_answers.append(cols[1])
+        data.append(cols)
+    return np.array(data), test_answers
+
+train_array = create_numpy_array(train_file)[0]
+test_array, test_answers = create_numpy_array(test_file)
+
+# create random forest object
+
+forest = RandomForestClassifier(n_estimators = 100)
+
+# fit training data
+
+#print train_array[0::,2::]
+
+forest = forest.fit(train_array[0::,2::],train_array[0::,1])
+
+output = forest.predict(test_array[0::,2::])
+
+correct = 0
+for result, answer in zip(output, test_answers):
+    if result == answer:
+        correct += 1
+
+print "test accuracy:", (float(correct) / float(len(output)))
+    
+    
+    
+    
+    
